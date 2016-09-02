04645602a65f6d562d365b151782c973bc402903
quangvinh93hcm <vinh.dq@felix.vn>
2016-09-02 16:04:13 +0700

    Create Entities

--- VinhCinema.Entities/Stock.cs
+++ VinhCinema.Entities/Stock.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace VinhCinema.Entities
 {
-    public class Stock
+    public class Stock: IEntityBase
     {
+        public Stock()
+        {
+            Rentals = new List<Rental>();
+        }
+        public int ID { get; set; }
+        public int MovieId { get; set; }
+        public virtual Movie Movie { get; set; }
+        public Guid UniqueKey { get; set; }
+        public bool IsAvailable { get; set; }
+        public virtual ICollection<Rental> Rentals { get; set; }
     }
 }
