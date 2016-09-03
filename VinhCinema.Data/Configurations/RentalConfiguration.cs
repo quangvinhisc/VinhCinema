fa302a6fae4666b8748e7ef688097f777bf4f26e
quangvinh93hcm <vinh.dq@felix.vn>
2016-09-03 12:33:42 +0700

    Initial Data Configurations

---
+++ VinhCinema.Data/Configurations/RentalConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VinhCinema.Entities;
+
+namespace VinhCinema.Data.Configurations
+{
+    public class RentalConfiguration : EntityBaseConfiguration<Rental>
+    {
+        public RentalConfiguration()
+        {
+            Property(r => r.CustomerID).IsRequired();
+            Property(r => r.StockID).IsRequired();
+            Property(r => r.Status).IsRequired().HasMaxLength(10);
+            Property(r => r.ReturnedDate).IsOptional();
+        }
+    }
+}
